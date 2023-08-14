c7c4b7410da73c96aeabd54143164f2823aecd66
fibbbonachos <arish2k3@gmail.com>
2023-08-14 14:47:45 +0530

    add: firebase

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,5 +1,7 @@
+import Home from "./pages/Home";
+
 function App() {
-  return <div className="text-xl">Homepage</div>;
+  return <Home />;
 }
 
 export default App;
---
+++ src/client/src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+  apiKey: "AIzaSyDoXx4X7zZUqQ2yaFrH735Um4NYHMqUf7o",
+  authDomain: "chatty-801e9.firebaseapp.com",
+  projectId: "chatty-801e9",
+  storageBucket: "chatty-801e9.appspot.com",
+  messagingSenderId: "806484821213",
+  appId: "1:806484821213:web:5c380f90915a41b5ff4734",
+  measurementId: "G-NE77KYDSYP",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
