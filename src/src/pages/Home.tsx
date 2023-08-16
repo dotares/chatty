b4e0d357a1f3a5b59609b8d75288b92c913ed88f
fibbbonachos <arish2k3@gmail.com>
2023-08-16 14:52:03 +0530

    add: only show auth when user is not signed up with useState hook

--- src/src/pages/Home.tsx
+++ src/src/pages/Home.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import SignUp from "./SignUp";
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
 
 const Home = () => {
-  return (
-    <div>
-      <SignUp />
-    </div>
-  );
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+
+  if (!isAuth) {
+    return (
+      <div>
+        <SignUp />
+      </div>
+    );
+  }
+
+  return <div>You're already signed up</div>;
 };
 
 export default Home;
