104e7373b956238b03bba54706d6b8b76c71c985
fibbbonachos <arish2k3@gmail.com>
2023-08-17 13:44:50 +0530

    add: messages snapshot listening for firestore and also display the messages temporarily on thewebsite

--- src/src/pages/Chat.tsx
+++ src/src/pages/Chat.tsx
@@ -1,11 +1,32 @@
-import { useState } from "react";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  where,
+  serverTimestamp,
+} from "firebase/firestore";
 import { auth, db } from "../firebase-config";
 
 const Chat = (props) => {
   const { room } = props;
   const [newMessage, setNewMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
   const messagesRef = collection(db, "messages");
+
+  useEffect(() => {
+    const queryMessages = query(messagesRef, where("room", "==", room));
+    onSnapshot(queryMessages, (snapshot) => {
+      let messagesArray = [];
+      snapshot.forEach((doc) => {
+        messagesArray.push({ ...doc.data(), key: doc.id });
+      });
+
+      setMessages(messagesArray);
+    });
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -23,6 +44,11 @@ const Chat = (props) => {
 
   return (
     <div className="fixed bottom-0 w-full">
+      <div>
+        {messages.map((message) => (
+          <h1>{message.text}</h1>
+        ))}
+      </div>
       <form className="flex" onSubmit={handleSubmit}>
         <input
           className="border-2 w-full border-black"
