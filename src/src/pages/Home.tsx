179458b13939913b54335c2feb66c230019e56cc
fibbbonachos <arish2k3@gmail.com>
2023-08-20 17:00:45 +0530

    update: input room name and button design and responsive

--- src/src/pages/Home.tsx
+++ src/src/pages/Home.tsx
@@ -104,21 +104,29 @@ const Home = () => {
               </div>
             </div>
           )}
-          <div className="my-12">
+          <div className="my-20 bg-[#5C5470] rounded-full p-4 flex justify-center drop-shadow-xl">
             <input
-              className=""
+              className="w-full bg-[#5C5470] text-[#FAF0E6] rounded placeholder:font-robotomono placeholder:text-[#B9B4C7] placeholder:px-2 outline-0 placeholder:text-sm"
+              placeholder="Enter room name ..."
               type="text"
               ref={roomInputRef}
-              placeholder="Enter room name"
             />
             <button
+              className="rounded px-2"
               onClick={() => {
                 if (roomInputRef.current) {
                   setRoom(roomInputRef.current.value);
                 }
               }}
             >
-              Enter chat
+              <svg
+                className="fill-[#FAF0E6]"
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 512 512"
+              >
+                <path d="M217.9 105.9L340.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L217.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1L32 320c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM352 416l64 0c17.7 0 32-14.3 32-32l0-256c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32l64 0c53 0 96 43 96 96l0 256c0 53-43 96-96 96l-64 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z" />
+              </svg>
             </button>
           </div>
         </div>
