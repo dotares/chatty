f34ffbe60aa371e2e4b6e8034e73a4add63bdb36
fibbbonachos <arish2k3@gmail.com>
2023-08-20 12:09:00 +0530

    update: signup page

--- src/src/pages/SignUp.tsx
+++ src/src/pages/SignUp.tsx
@@ -1,6 +1,7 @@
 import { auth, provider } from "../firebase-config";
 import { signInWithPopup } from "firebase/auth";
 import Cookies from "universal-cookie";
+import Footer from "../components/Footer";
 
 const cookies = new Cookies();
 
@@ -19,14 +20,34 @@ const SignUp = (props: Props) => {
     }
   };
   return (
-    <div>
-      <p>Sign Up with Google</p>
-      <button
-        className="border-black border-2 rounded"
-        onClick={signInWithGoogle}
-      >
-        Sign up
-      </button>
+    <div className="text-center">
+      <div className="flex flex-col text-center h-screen">
+        <div className="m-auto">
+          <div>
+            <h1 className="py-12 font-pacifico text-8xl text-[#FAF0E6] drop-shadow-xl">
+              Chatty
+            </h1>
+          </div>
+          <div>
+            <button
+              className="transition hover:scale-110 rounded-full p-2 text-4xl bg-[#5C5470] hover:bg-[#B9B4C7] drop-shadow-xl hover:drop-shadow-none"
+              onClick={signInWithGoogle}
+            >
+              <svg
+                className="transition hover:scale-110 fill-[#B9B4C7] hover:fill-[#5C5470]"
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 488 512"
+              >
+                <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 };
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pacifico: ["Pacifico"],
+        robotomono: ["Roboto Mono"],
+      },
+    },
   },
   plugins: [],
 };
