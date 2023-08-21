ce0cf4430695c64ce92f6dd759762b3a48dfdb69
fibbbonachos <arish2k3@gmail.com>
2023-08-21 14:10:00 +0530

    update: design the chat room page excluding messages

--- src/src/pages/Chat.tsx
+++ src/src/pages/Chat.tsx
@@ -9,9 +9,12 @@ import {
   orderBy,
 } from "firebase/firestore";
 import { auth, db } from "../firebase-config";
+import SignOut from "../components/SignOut";
 
 interface Props {
   room: string;
+  resetRoom: Function;
+  signUserOut: Function;
 }
 
 interface Message {
@@ -62,8 +65,25 @@ const Chat = (props: Props) => {
 
   return (
     <div>
-      <div className="font-pacifico text-[#FAF0E6] p-4">
-        <h1 className="text-4xl text-center">{props.room}</h1>
+      <div className="flex flex-row justify-between">
+        <div className="p-8">
+          <button onClick={() => props.resetRoom()}>
+            <svg
+              className="fill-[#FAF0E6] text-2xl"
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 384 512"
+            >
+              <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z" />
+            </svg>
+          </button>
+        </div>
+        <div className="font-pacifico text-[#FAF0E6] p-6">
+          <h1 className="text-4xl text-center">{props.room}</h1>
+        </div>
+        <div className="p-8">
+          <SignOut signUserOut={props.signUserOut} />
+        </div>
       </div>
       <div className="fixed bottom-0 w-full">
         <div>
@@ -85,7 +105,7 @@ const Chat = (props: Props) => {
             />
             <button className="px-4" type="submit">
               <svg
-                className="fill-[#FAF0E6]"
+                className="fill-[#FAF0E6] text-2xl"
                 xmlns="http://www.w3.org/2000/svg"
                 height="1em"
                 viewBox="0 0 512 512"
