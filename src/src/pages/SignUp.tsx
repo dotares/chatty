73716a9789a7420ba5785ebf64752f1cb55ed3ac
fibbbonachos <arish2k3@gmail.com>
2023-08-28 17:07:59 +0530

    update: handle authentication component

--- src/src/pages/SignUp.tsx
+++ src/src/pages/SignUp.tsx
@@ -2,6 +2,7 @@ import { auth, provider } from "../firebase-config";
 import { signInWithPopup } from "firebase/auth";
 import Cookies from "universal-cookie";
 import Footer from "../components/Footer";
+import Authentication from "../components/Authentication";
 
 const cookies = new Cookies();
 
@@ -32,28 +33,7 @@ const SignUp = (props: Props) => {
             </p>
           </div>
           {/* Authentication Section */}
-          <div>
-            <button
-              className="group transition hover:scale-110 rounded-xl w-2/3 p-4 text-md bg-[#5C5470] hover:bg-[#B9B4C7] drop-shadow-xl hover:drop-shadow-none"
-              onClick={signInWithGoogle}
-            >
-              <div className="flex font-rubik items-center justify-center space-x-4">
-                <div>
-                  <svg
-                    className="transition group-hover:scale-110 fill-[#B9B4C7] group-hover:fill-[#352F44]"
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="1.5em"
-                    viewBox="0 0 488 512"
-                  >
-                    <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
-                  </svg>
-                </div>
-                <div className="transition group-hover:text-[#352F44] text-[#FAF0E6]">
-                  Sign in with Google
-                </div>
-              </div>
-            </button>
-          </div>
+          <Authentication signInWithGoogle={signInWithGoogle} />
         </div>
       </div>
       <div>
