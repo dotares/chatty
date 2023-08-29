13ea110b1eadda87a973f003bf9099eaccf017b2
fibbbonachos <arish2k3@gmail.com>
2023-08-29 23:15:37 +0530

    update: clean

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,8 +1,6 @@
 import Home from "./pages/Home";
-import { invoke } from "@tauri-apps/api";
 
 function App() {
-  invoke("greet", { name: "World" }).then((response) => console.log(response));
   return <Home />;
 }
 
